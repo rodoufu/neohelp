2b76e25fd7c3213d2ed3ee92d500926b9e2432c5
Rodolfo Araujo <rodoufu@gmail.com>
2018-11-15 16:46:49 -0200

    Ensuring that is using valid trees for the tests.

--- NeoDataStructureTest/MerklePatriciaTest.cs
+++ NeoDataStructureTest/MerklePatriciaTest.cs
@@ -32,13 +32,17 @@ namespace com.github.neoresearch.NeoDataStructureTest
 
             InserirTestar("01a2", "valor1");
             Assert.Equal(1, merklePatricia.Height());
+            Assert.True(merklePatricia.Validade());
 
             InserirTestar("11a2", "valor2");
+            Assert.True(merklePatricia.Validade());
 
             InserirTestar("0212", "valor3");
             Assert.Equal(3, merklePatricia.Height());
+            Assert.True(merklePatricia.Validade());
 
             merklePatricia["0"] = "valor4";
+            Assert.True(merklePatricia.Validade());
         }
 
         [Fact]
@@ -51,28 +55,35 @@ namespace com.github.neoresearch.NeoDataStructureTest
             };
             Assert.True(merklePatricia.ContainsKey("oi"));
             Assert.Equal("batatatinha", merklePatricia["oi"]);
+            Assert.True(merklePatricia.Validade());
 
             merklePatricia["orelha"] = "batatatinha";
             Assert.Equal("batatatinha", merklePatricia["orelha"]);
+            Assert.True(merklePatricia.Validade());
 
             merklePatricia["orfão"] = "criança";
             Assert.Equal("criança", merklePatricia["orfão"]);
+            Assert.True(merklePatricia.Validade());
 
             merklePatricia["orfanato"] = "crianças";
             Assert.Equal("crianças", merklePatricia["orfanato"]);
+            Assert.True(merklePatricia.Validade());
 
             merklePatricia.Remove("orfanato");
             Assert.Equal("criança", merklePatricia["orfão"]);
             Assert.False(merklePatricia.ContainsKey("orfanato"));
+            Assert.True(merklePatricia.Validade());
 
             merklePatricia["orfã"] = "menina";
             Assert.Equal("menina", merklePatricia["orfã"]);
+            Assert.True(merklePatricia.Validade());
         }
 
         [Fact]
         public void Remove()
         {
             var mp = new MerklePatricia();
+            Assert.True(mp.Validade());
 
             void RemoverTestar(string x, string y)
             {
@@ -87,24 +98,30 @@ namespace com.github.neoresearch.NeoDataStructureTest
             }
 
             RemoverTestar("oi", "bala");
+            Assert.True(mp.Validade());
             mp.Remove("oi");
             Assert.False(mp.ContainsKey("oi"));
+            Assert.True(mp.Validade());
 
             mp["123"] = "abc";
             mp["a123"] = "1abc";
             Assert.Equal(2, mp.Count());
-            
+            Assert.True(mp.Validade());
+
             Assert.False(mp.Remove("b123"));
             Assert.Equal(2, mp.Count());
             Assert.True(mp.Remove("a123"));
-            // TODO Solve problem on the remove method, not removing from _db
+            Assert.True(mp.Validade());
             Assert.Equal(1, mp.Count());
             Assert.False(mp.ContainsKey("a123"));
             Assert.True(mp.ContainsKey("123"));
             Assert.True(mp.ContainsKey("123"));
+            Assert.True(mp.Validade());
 
             var mp2 = new MerklePatricia {["123"] = "abc"};
             Assert.Equal(mp2, mp);
+            Assert.True(mp.Validade());
+            Assert.True(mp2.Validade());
         }
 
         [Fact]
@@ -117,6 +134,7 @@ namespace com.github.neoresearch.NeoDataStructureTest
                 ["oi2"] = "b2ola",
                 ["oi1"] = "bola1"
             };
+            Assert.True(mpA.Validade());
 
             var mpB = new MerklePatricia
             {
@@ -125,31 +143,38 @@ namespace com.github.neoresearch.NeoDataStructureTest
                 ["oi2"] = "b2ola",
                 ["oi1"] = "bola1"
             };
+            Assert.True(mpB.Validade());
             Assert.Equal(mpA, mpB);
 
             mpA["oi"] = "escola";
             Assert.NotEqual(mpA, mpB);
+            Assert.True(mpA.Validade());
 
             mpB["oi"] = "escola";
             Assert.Equal(mpA, mpB);
+            Assert.True(mpB.Validade());
 
             mpA["oi123"] = "escola";
             mpA["oi12"] = "escola1";
             mpA["bola"] = "escola2";
             mpA["dog"] = "escola2";
+            Assert.True(mpA.Validade());
 
             mpB["bola"] = "escola2";
             mpB["dog"] = "escola2";
             mpB["oi12"] = "escola1";
             mpB["oi123"] = "escola";
             Assert.Equal(mpA, mpB);
+            Assert.True(mpB.Validade());
 
             mpA.Remove("oi");
             mpA.Remove("oi");
             Assert.NotEqual(mpA, mpB);
+            Assert.True(mpA.Validade());
 
             mpB.Remove("oi");
             Assert.Equal(mpA, mpB);
+            Assert.True(mpB.Validade());
         }
 
         [Fact]
@@ -161,6 +186,7 @@ namespace com.github.neoresearch.NeoDataStructureTest
             {
                 mp[it] = it.CompactEncodeString();
                 System.Console.WriteLine($"{mp}");
+            Assert.True(mp.Validade());
             }
         }
 
@@ -184,25 +210,30 @@ namespace com.github.neoresearch.NeoDataStructureTest
             {
                 merklePatricia[keyValue.Key] = keyValue.Value;
             }
+            Assert.True(merklePatricia.Validade());
 
             foreach (var keyValue in exemplo)
             {
                 Assert.Equal(keyValue.Value, merklePatricia[keyValue.Key]);
             }
+            Assert.True(merklePatricia.Validade());
         }
 
         [Fact]
         public void PatriciaToString()
         {
             var mp = new MerklePatricia();
+            Assert.True(mp.Validade());
             System.Console.WriteLine($"a:\n {mp}");
             mp["oi"] = "bala";
+            Assert.True(mp.Validade());
             System.Console.WriteLine($"a:\n {mp}");
             mp["oi1"] = "bala1";
             System.Console.WriteLine($"a:\n {mp}");
             mp["oi2"] = "bala2";
             System.Console.WriteLine($"a:\n {mp}");
             mp["oi12"] = "bala12";
+            Assert.True(mp.Validade());
             System.Console.WriteLine($"a:\n {mp}");
             mp["bola"] = "123bala12";
             System.Console.WriteLine($"a:\n {mp}");
@@ -210,6 +241,7 @@ namespace com.github.neoresearch.NeoDataStructureTest
             System.Console.WriteLine($"a:\n {mp}");
             mp["ca123"] = "3bala12";
             mp["oi123"] = "asfbala12";
+            Assert.True(mp.Validade());
             System.Console.WriteLine($"a:\n {mp}");
         }
 
@@ -218,22 +250,26 @@ namespace com.github.neoresearch.NeoDataStructureTest
         {
             var mp = new MerklePatricia();
             Assert.Equal(0, mp.Count());
-            
+            Assert.True(mp.Validade());
+
             mp["oi"] = "oi";
             Assert.Equal(1, mp.Count());
-            
+            Assert.True(mp.Validade());
+
             mp["oi"] = "oi";
             Assert.Equal(1, mp.Count());
-            
+            Assert.True(mp.Validade());
+
             mp["oi"] = "oi1";
             Assert.Equal(1, mp.Count());
-            
+
             mp["oi1"] = "oi1";
             mp["oi2"] = "oi2";
             Assert.Equal(3, mp.Count());
-            
+
             mp["bala"] = "bala2";
             Assert.Equal(4, mp.Count());
+            Assert.True(mp.Validade());
         }
 
         [Fact]
@@ -245,13 +281,15 @@ namespace com.github.neoresearch.NeoDataStructureTest
                 ["boi2"] = "oi2",
                 ["coi1"] = "oi3"
             };
+            Assert.True(mp.Validade());
             Assert.True(mp.ContainsValue("oi"));
             Assert.True(mp.ContainsValue("oi2"));
             Assert.True(mp.ContainsValue("oi3"));
-            
+
             Assert.False(mp.ContainsValue("aoi"));
             Assert.False(mp.ContainsValue("boi2"));
             Assert.False(mp.ContainsValue("coi3"));
+            Assert.True(mp.Validade());
         }
     }
 }
